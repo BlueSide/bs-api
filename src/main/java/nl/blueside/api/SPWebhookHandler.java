83c445c43db9a24df3a4d5573879f29b7d8fdfd1
cikzh <marlon@blueside.nl>
2017-12-01 19:00:27 +0100

    properly implemented validation endpoint

--- src/main/java/nl/blueside/api/SPWebhookHandler.java
+++ src/main/java/nl/blueside/api/SPWebhookHandler.java
@@ -3,6 +3,7 @@ package nl.blueside.api;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 import java.net.URISyntaxException;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +15,11 @@ import org.springframework.http.HttpStatus;
 public class SPWebhookHandler
 {
 
-    @RequestMapping(method = { RequestMethod.POST })
-    private ResponseEntity<String> copy(HttpServletRequest request) throws URISyntaxException
+    @RequestMapping(method = { RequestMethod.POST }, params = {"validationToken"})
+    
+    private ResponseEntity<String> validate(@RequestParam(value = "validationToken") String validateToken) throws URISyntaxException
     {
-        System.out.println(request.toString());
-        return new ResponseEntity<String>(HttpStatus.OK);
+        System.out.println(validateToken);
+        return new ResponseEntity<String>(validateToken, HttpStatus.OK);
     }
 }
